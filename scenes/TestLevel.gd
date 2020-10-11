6b39eb24fcba253b0bc625156b56fb8fcb70274a
Alexander Egorov <amego2006@gmail.com>
2020-10-11 18:25:59 +0300

    Added "attack" hotkey

--- scenes/TestLevel.gd
+++ scenes/TestLevel.gd
@@ -4,6 +4,8 @@ var currentSelection
 
 onready var entities = $Entities
 
+var currentHandAction
+
 func _ready():
 	var unit
 	
@@ -12,42 +14,36 @@ func _ready():
 		unit.position = Vector2(300 + randi() % 50, 250 + i*40)
 		add_entity(unit)
 
-#	unit = UnitFactory.createClubman(Entity.TEAM_PLAYER)
-#	unit.position = Vector2(330, 450)
-#	add_entity(unit)
-#
-#	unit = UnitFactory.createClubman(Entity.TEAM_PLAYER)
-#	unit.position = Vector2(300, 400)
-#	add_entity(unit)
-	
 	for i in range(8):
 		unit = UnitFactory.createClubman(Entity.TEAM_ENEMY)
 		unit.position = Vector2(800 + randi() % 50, 250 + i*40)
 		add_entity(unit)
 	
-#	unit = UnitFactory.createClubman(Entity.TEAM_ENEMY)
-#	unit.position = Vector2(1000, 350)
-#	add_entity(unit)
-#
-#	unit = UnitFactory.createClubman(Entity.TEAM_ENEMY)
-#	unit.position = Vector2(1000, 450)
-#	add_entity(unit)
-	
-	# unit.perform_action("Move", {"target": Vector2(500, 500)})
 	currentSelection = Selection.new()
 	currentSelection.level = self
 
 func _input(event):
+	if event is InputEventKey:
+		if event.scancode == KEY_A:
+			currentHandAction = "Attack"
+		elif event.scancode == KEY_ESCAPE:
+			currentHandAction = null
+			
 	if event is InputEventMouse:
 		if event.button_mask & BUTTON_LEFT:
-			if event.is_pressed():
-				if !currentSelection.selectionStartPos:
-					currentSelection.startSelection(event.position)
+			if !currentHandAction:
+				if event.is_pressed():
+					if !currentSelection.selectionStartPos:
+						currentSelection.startSelection(event.position)
+				else:
+					if currentSelection.selectionStartPos:
+						currentSelection.continueSelection(event.position)
 			else:
-				if currentSelection.selectionStartPos:
-					currentSelection.continueSelection(event.position)
+				currentSelection.send_action_to_entities(currentHandAction, {"target": event.position})
+				currentHandAction = null		
 		elif currentSelection.selectionStartPos:
-			currentSelection.endSelection(event.position)
+			if currentSelection.selectionStartPos:
+				currentSelection.endSelection(event.position)
 			
 		if event.button_mask & BUTTON_RIGHT:
 			if event.is_pressed() && currentSelection.selectedEntities.size() > 0:
@@ -58,7 +54,7 @@ func _input(event):
 					elif entity.defaultTargetAction:
 						currentSelection.send_action_to_entities(entity.defaultTargetAction, {"target": entity})
 				else:
-					currentSelection.send_action_to_entities("Attack", {"target": event.position})
+					currentSelection.send_action_to_entities("Move", {"target": event.position})
 
 func add_entity(entity):
 	entities.add_child(entity)
